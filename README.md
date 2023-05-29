3cc92434bbbf82f1c17886c4199ac2d0828105e8
kgugnani <kgugnani@ucsd.edu>
2023-05-28 23:24:01 -0700

    FINAL COMMIT

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lab 8 - Starter
+Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why. <br>
+
+I would choose to run the tests within a GitHub Action so everytime some code is pushed to the repository the tests are run. I believe running the tests manually is a step that is easy to forget and could cause a lot of problems if not documented properly, while running the tests automatically would ensure that the tests are run everytime the code is pushed to the repository. <br>
+
+Would you use an end to end test to check if a function is returning the correct output?
+
+No, I would use a unit test to check if a function is returning the correct output as end to end testing is more related to making sure your front-end works and simulating a user, not just checking outputs of functions. <br>
+
+Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+Yes, I would use a unit test to make sure this feature works properly. I would use a unit test as it is important to indivudally test each function/feature and this one can easily be checked. 
+
+Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, I would use a unit test as this feature can easily be checked with a test and it is good to make sure all of your features work individually and alongside the other features. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Phone number true1', () => {
+    expect(functions.isPhoneNumber('244-392-2323')).toBe(true);
+});
+
+test('Phone number true2', () => {
+    expect(functions.isPhoneNumber('134-346-4573')).toBe(true);
+});
+
+test('Phone number false1', () => {
+    expect(functions.isPhoneNumber('WHADDUP')).toBe(false);
+});
+
+test('Phone number false2', () => {
+    expect(functions.isPhoneNumber('55555-555-5')).toBe(false);
+});
+
+test('Email true1', () => {
+    expect(functions.isEmail('gugnanikartik23@gmail.com')).toBe(true);
+});
+
+test('Email true2', () => {
+    expect(functions.isEmail('gugnanik23@gmail.com')).toBe(true);
+});
+
+test('Email false1', () => {
+    expect(functions.isEmail('WHADDUP')).toBe(false);
+});
+
+test('Email false2', () => {
+    expect(functions.isEmail('55555-555-5')).toBe(false);
+});
+
+test('Strong password true1', () => {
+    expect(functions.isStrongPassword('Lost_inthesauce')).toBe(true);
+});
+
+test('Strong password true2', () => {
+    expect(functions.isStrongPassword('WhydidIdo_this')).toBe(true);
+});
+
+test('Strong password false1', () => {
+    expect(functions.isStrongPassword('pc')).toBe(false);
+});
+
+test('Strong password false2', () => {
+    expect(functions.isStrongPassword('p')).toBe(false);
+});
+
+test('Date true1', () => {
+    expect(functions.isDate('12/23/2002')).toBe(true);
+});
+
+test('Date true2', () => {
+    expect(functions.isDate('07/13/2021')).toBe(true);
+});
+
+test('Date false1', () => {
+    expect(functions.isDate('WHADDUP')).toBe(false);
+});
+
+test('Date false2', () => {
+    expect(functions.isDate('55555-555-5')).toBe(false);
+});
+
+test('Hex true1', () => {
+    expect(functions.isHexColor('#9F2B68')).toBe(true);
+});
+
+test('Hex true2', () => {
+    expect(functions.isHexColor('#800020')).toBe(true);
+});
+
+test('Hex false1', () => {
+    expect(functions.isHexColor('WHADDUP')).toBe(false);
+});
+
+test('Hex false2', () => {
+    expect(functions.isHexColor('55555-555-5')).toBe(false);
+});
